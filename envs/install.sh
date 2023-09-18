5ea1306c1c24cdb14bd6f13913f16b45d5494031
anupamajha1 <anupamaj@uw.edu>
2023-09-18 13:36:27 -0700

    alternate to conda yaml, much faster

---
+++ envs/install.sh
@@ -0,0 +1,17 @@
+conda create --name ont_m6a
+
+conda activate ont_m6a
+
+conda install -c anaconda python=3.8
+
+conda install -c conda-forge matplotlib
+
+#conda install -c conda-forge numpy
+
+conda install -c anaconda scipy
+
+conda install -c anaconda scikit-learn
+
+conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia
+
+
