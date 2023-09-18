e28a96fb96e9da0ee3dbc9d4f473adba69cee385
anupamajha1 <anupamaj@uw.edu>
2023-09-17 19:49:51 -0700

    script for copying data over

---
+++ data/get_data.sh
@@ -0,0 +1,3 @@
+cp /net/gs/vol4/shared/public/hackathon_2023/Stergachis_lab/data/ml_data/HG002_2_3_00_train.npz .
+cp /net/gs/vol4/shared/public/hackathon_2023/Stergachis_lab/data/ml_data/HG002_2_3_00_val.npz .
+cp /net/gs/vol4/shared/public/hackathon_2023/Stergachis_lab/data/ml_data/HG002_2_3_00_test.npz .
